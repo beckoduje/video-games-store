1fcd7c889a460762f2e412abad3566e40c84ba0c
beckoduje <dujebecko@gmail.com>
2022-04-11 22:45:51 +0300

    11/04/2022

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Navigation from "./components/Navigation";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navigation />
+      <Routes>
+        {/* <Route path="/rockets" element={<Welcome />} /> 
+        <Route path="/about" element={<About />} > */}
+        <Route path="/" element={<Navigate replace to="/home" />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Navigation.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import SearchIcon from "@mui/icons-material/Search";
+
+export default function Navigation() {
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Box sx={{ marginRight: "auto" }}>livo</Box>
+        <Box sx={{ display: "flex", alignItems: "center" }}>
+          <Button variant="contained">Sign In</Button>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Avatar
+              alt="User image"
+              //src="https://sfatulparintilor.ro/wp-content/uploads/2018/01/rase-de-caini-sfatulparintilor.ro-pixabay_com-alaskan-malamute-1531351_1920.jpg"
+            />
+            <IconButton>
+              <Badge badgeContent={4} color="error">
+                <FavoriteBorderIcon />
+              </Badge>
+            </IconButton>
+          </Box>
+          <SearchIcon />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+}
